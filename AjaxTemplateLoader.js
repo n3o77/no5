665fe8b1ba12de4c7c06a40312e354cecc55ff61
Steffen Persch <s.persch@unlooped.com>
2013-12-26 22:16:52 +0100

    [REF] updated AjaxTemplateLoader

--- AjaxTemplateLoader.js
+++ AjaxTemplateLoader.js
@@ -8,14 +8,19 @@ var agent = require('agent');
 
 var AjaxTemplateLoader = prime({
 
+    config: {},
+
 	constructor: function (conf) {
-		this.config = object.mixIn(config, conf)
+		this.config = object.mixIn(this.config, conf)
 	},
 
 	loadTemplate: function(filePath) {
-		return Promise.denodeify(agent)('get', {'template': filePath}, this.config.loaderUrl);
+        return new Promise(function(resolve, reject) {
+            agent('GET', this.config.loaderUrl, {'template': filePath}, function(response) {
+                return resolve(response.body);
+            });
+        }.bind(this));
 	}
-
 });
 
 module.exports = AjaxTemplateLoader

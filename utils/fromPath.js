2fbc70216c2a6b68c459599f6ac6943e0d803040
Steffen Persch <s.persch@unlooped.com>
2013-12-29 01:51:34 +0100

    [REF] don't check property with hasOwn

--- utils/fromPath.js
+++ utils/fromPath.js
@@ -1,11 +1,9 @@
 "use strict";
 
-var hasOwn = require('prime/object/hasOwn')
-
 function fromPath(source, parts) {
     if (typeof parts == 'string') parts = parts.split('.')
     for (var i = 0, l = parts.length; i < l; i++) {
-        if (hasOwn(source, parts[i])) source = source[parts[i]]
+        if (source[parts[i]]) source = source[parts[i]]
         else return null
     }
     return source

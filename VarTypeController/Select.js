7becd3317b8b58e45284208c2814b9b84945aae8
Steffen Persch <s.persch@unlooped.com>
2013-12-30 17:05:19 +0100

    [REF] fixes + code convention

--- VarTypeController/Select.js
+++ VarTypeController/Select.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var prime = require('prime');
+var prime = require('prime')
 var Promise = require('promise')
-var fromPath = require('../utils/fromPath');
-var typeOf = require('prime/type');
-var tplItem = require('../item');
-var Partial = require('./Partial');
+var fromPath = require('../utils/fromPath')
+var typeOf = require('prime/type')
+var tplItem = require('../item')
+var Partial = require('./Partial')
 var array = {
     'forEach': require('prime/array/forEach'),
     'map': require('prime/array/map')
-};
+}
 var object = {
     'forOwn': require('prime/object/forOwn'),
     'mixIn': require('prime/object/mixIn')
-};
+}
 
 
 var Text = prime({
@@ -25,38 +25,37 @@ var Text = prime({
     },
 
     render: function() {
-        var options = fromPath(this.tplDesc.values, this.varTypeTag.name);
-        var tpl = fromPath(this.tplDesc.values, this.varTypeTag.tpl);
+        var options = fromPath(this.tplDesc.values, this.varTypeTag.name)
+        var tpl = fromPath(this.tplDesc.values, this.varTypeTag.tpl)
         var values = {
             'options': []
-        };
-
-        if (typeOf(options) === 'object') {
-            options = this.generateOptionsFromObject();
         }
 
-        values.options = this.buildOptions(options);
+        if (typeOf(options) === 'object') {
+            options = this.generateOptionsFromObject()
+        }
 
-        return this.templateController.getTemplateParser().parse(tplItem(tpl || 'input/select', object.mixIn({}, this.varTypeTag, values)));
+        values.options = this.buildOptions(options)
+        return this.templateController.getTemplateParser().parse(tplItem(tpl || 'input/select', object.mixIn({}, this.varTypeTag, values)))
     },
 
     generateOptionsFromObject: function(options) {
-        var opts = [];
+        var opts = []
         forOwn(options, function(value, key) {
-            opts.push({'value': key, 'label': value});
-        });
+            opts.push({'value': key, 'label': value})
+        })
 
-        return opts;
+        return opts
     },
 
     buildOptions: function(options) {
-        if (!options) return [];
-        var value = fromPath(this.tplDesc.values, this.varTypeTag.value);
-        return array.map(options, function(option) {
-            if (options.value === value) option.selected = 'selected';
-            return tplItem('input/option', option);
-        });
+        if (!options) return []
+        var value = fromPath(this.tplDesc.values, this.varTypeTag.value)
+        return array.map(options, function(option, index) {
+            if (option.value === value) option.selected = 'selected'
+            return tplItem('input/option', option, index)
+        })
     }
-});
+})
 
 module.exports = Text

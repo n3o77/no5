c201bed1e80b0d2475e0ba6d3188cc0f384af29e
Steffen Persch <s.persch@unlooped.com>
2014-01-28 17:52:03 +0100

    [ENH] added cache to templateLoader. Closes #1

--- TemplateLoader.js
+++ TemplateLoader.js
@@ -12,11 +12,15 @@ var TemplateLoader = prime({
 
 	config: {
 		'templates': null,
-		'tplSuffix': 'html'
+		'tplSuffix': 'html',
+        'cache': false
 	},
+
+    cache: null,
 
 	constructor: function (conf) {
 		this.config = object.mixIn(config, conf)
+        this.cache = {}
 	},
 
 	loadTemplate: function(filePath) {
@@ -26,10 +30,15 @@ var TemplateLoader = prime({
 			filePath = path.normalize(config.templates + '/' + filePath + '.' + config.tplSuffix)
 			if (!fs.existsSync(filePath) && fs.existsSync(fallBack)) filePath = fallBack
 
+            if (this.config.cache && this.cache[filePath]) return resolve(this.cache[filePath])
+
 			fs.readFile(filePath, 'utf-8', function(err, data) {
 				if (err) return reject(err)
+
+                if (this.config.cache) this.cache[filePath] = data
+
 				resolve(data)
-			})
+            }.bind(this))
 		}.bind(this));
 	}
 

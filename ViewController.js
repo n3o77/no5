850bbbbed1893bd4d5aeb06dbd5da295c29e5930
Steffen Persch <s.persch@unlooped.com>
2014-01-28 17:19:26 +0100

    [ENH] added ViewController Base

---
+++ ViewController.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var prime = require('prime')
+var object = {
+    'merge': require('mout/object/merge')
+};
+
+var ViewController = prime({
+
+    constructor: function (objVar, item, templateController, options) {
+        this.options = object.merge(this.options, options)
+        this.objVar = objVar
+        this.varTypeTag = objVar.tplVar
+        this.item = item
+        this.templateController = templateController
+    },
+
+    parse: function() {
+        throw Error('Parse method not implemented in ViewController: ' + this.varTypeTag.type + ' From:' + this.item.template + ':' + this.objVar.pos.line + ':' + this.objVar.pos.col);
+    }
+
+})
+
+module.exports = ViewController
